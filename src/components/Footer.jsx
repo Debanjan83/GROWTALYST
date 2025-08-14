f2cba8c1a01820c8f9cd41173afa961a064be247
Debanjan Pal <debanjanpal79611@gmail.com>
2025-08-14 22:00:29 +0530

    Files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-black backdrop-blur-md p-4 mt-10 flex flex-col sm:flex-row items-center justify-between gap-3 sm:gap-7'>
+    <div className='bg-black backdrop-blur-md p-4 mt-10 flex flex-col sm:flex-row items-center justify-around gap-3 sm:gap-7'>
       <div className='text-white text-center sm:text-left'>&copy; All Rights Reserved</div>
       <div className='text-white text-center sm:text-right'>Powered by Growtalyst</div>
     </div>
